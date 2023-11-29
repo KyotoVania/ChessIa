37b516f0e1bee0ff921e742efe7efcac8170e902
Tokyovania <jean-cyprien.roux@epitech.eu>
2023-11-29 16:08:54 +0100

    [ADD] STEP 4 upgrade and reade

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# ChessIa
-workshop
+# Chess AI Project
+
+## Description
+The Chess AI project is a Python-based application that leverages neural network capabilities using PyTorch. It offers an engaging platform for users to play chess against an AI, observe AI vs. AI games, or compete against an AI with random moves.
+
+## Features
+- **Play Chess Against an AI**: Test your skills by playing against a computer-controlled opponent.
+- **AI vs. AI**: Sit back and watch as two AI opponents battle it out on the chessboard.
+- **Random Move AI**: Enjoy a less predictable game by playing against an AI that makes random moves.
+- **Continuous Game Mode**: Play games continuously with the option to pause, change modes, or exit at any time.
+- **Verbose Mode**: Get detailed game output for an in-depth understanding of each move.
+
+## Requirements
+- Python 3.x
+- PyTorch
+- python-chess library
+
+## Installation
+1. **Clone the Repository**:
+   ```bash
+   git clone [URL]
+
+2. **Install Required Packages**:
+   ```bash
+   pip install torch chess
+
+## Usage
+
+Run the program from the command line, specifying the mode and additional options:
+
+``` bash 
+python chess_ai.py --mode [MODE] [--continuous] [--verbose]
+
+Modes
+ai_vs_human: Engage in a strategic game against the AI.
+ai_vs_ai: Watch an automated chess match between two AI players.
+ai_vs_random: Challenge an AI opponent that moves randomly.
+Options
+--continuous: Continuously play games until manually stopped. Options to continue, change the mode, or exit are available after stopping.
+--verbose: Enable verbose mode for detailed game insights, including the chessboard state after each move.
+```
+
+## Examples
+Play a Single Game Against the AI:
+```bash
+python chess_ai.py --mode ai_vs_human
+```
+Run Continuous AI vs. AI Games:
+```bash
+python chess_ai.py --mode ai_vs_ai --continuous
+```
+Play a Verbose Game Against a Random-Moving AI:
+```bash
+python chess_ai.py --mode ai_vs_random --verbose
--- chessStep4.py
+++ chessStep4.py
@@ -8,9 +8,8 @@ import torch.nn.functional as F
 class ChessAI(nn.Module):
     def __init__(self):
         super(ChessAI, self).__init__()
-        # Définir les couches ici
-        self.fc1 = nn.Linear(64, 128)  # 64 entrées pour chaque case, 128 neurones dans la couche cachée
-        self.fc2 = nn.Linear(128, 64)  # 64 sorties pour chaque case
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 64)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -19,7 +18,6 @@ class ChessAI(nn.Module):
 
 
 
-# Example function to convert board state to model input
 def board_to_input(board):
     piece_to_value = {
         chess.PAWN: 1,
@@ -34,7 +32,6 @@ def board_to_input(board):
     for i in range(64):
         piece = board.piece_at(i)
         if piece is not None:
-            # Assign a value based on piece type and color
             value = piece_to_value.get(piece.piece_type, 0)
             if piece.color == chess.BLACK:
                 value = -value
@@ -43,12 +40,10 @@ def board_to_input(board):
     return board_state
 
 
-# Example function to choose a move from model's output
 def choose_move_from_output(output, board):
-    # Flatten the output and sort it
     sorted_indices = torch.argsort(output, descending=True).view(-1)
 
-    # Iterate over sorted indices to find a legal move
+
     for i in range(0, len(sorted_indices), 2):
         from_square = sorted_indices[i].item()
         to_square = sorted_indices[i + 1].item() if i + 1 < len(sorted_indices) else -1
@@ -56,11 +51,9 @@ def choose_move_from_output(output, board):
         if move in board.legal_moves:
             return move
 
-    # If no legal move found, return a random legal move
     return random.choice(list(board.legal_moves))
 
 
-# Main function to play a game
 def play_chess_game(ai_model, mode='ai_vs_human'):
     board = chess.Board()
     while not board.is_game_over():
@@ -89,31 +82,87 @@ def play_chess_game(ai_model, mode='ai_vs_human'):
             else:  # Random's turn
                 move = random.choice(list(board.legal_moves))
 
-        # Check if the move is legal and execute it
         if move in board.legal_moves:
             board.push(move)
         else:
             print("Illegal move, try again." if mode == 'ai_vs_human' else "Invalid move selected.")
             continue
-
-        print(board)
+        if args.verbose:
+            print(board)
+            print("===========================================================================================================")
 
     print("===========================================================================================================")
-    print("Game over.")
-    print("Reason:", board.result())
     print(board)
+    result = board.result(claim_draw=True)
+
+    # Additional checks for game end conditions
+    if result == "1/2-1/2" or result == "1-0" or result == "0-1":
+        return result
+    elif board.is_checkmate():
+        return "1-0" if board.turn == chess.BLACK else "0-1"
+    elif board.can_claim_draw():
+        return "1/2-1/2"
+    elif board.is_stalemate() or board.is_insufficient_material() or board.is_seventyfive_moves() or board.is_fivefold_repetition():
+        return "1/2-1/2"
+    else:
+        return "1/2-1/2"
 
 
-# Example usage
-chess_model = ChessAI()
 
-parser = argparse.ArgumentParser(description='Play Chess Game')
-parser.add_argument('--mode', type=str, default='ai_vs_human',
-                    choices=['ai_vs_human', 'ai_vs_ai', 'ai_vs_random'],
-                    help='Choose game mode: ai_vs_human, ai_vs_ai, or ai_vs_random')
-args = parser.parse_args()
+def run_continuous_games(ai_model, initial_mode='ai_vs_ai', verbose=False):
+    mode = initial_mode
+    results = {"1-0": 0, "0-1": 0, "1/2-1/2": 0}
+    continue_running = True
 
-# Instantiate the model
-chess_model = ChessAI()
+    while continue_running:
+        try:
+            print("Starting new game in mode:", mode)
+            result = play_chess_game(ai_model, mode=mode)
+            results[result] += 1
+            print(f"Game result: {result}. Total results: {results}")
 
-play_chess_game(chess_model, mode=args.mode)
+        except KeyboardInterrupt:
+            print("\nInterrupted!")
+            print("Current Results:")
+            print(results)
+            print("\nChoose an option:")
+            print("1: Continue")
+            print("2: Change Mode")
+            print("3: Exit")
+            choice = input("Enter your choice (1, 2, or 3): ")
+
+            if choice == '1':
+                # Continue the loop
+                continue
+            elif choice == '2':
+                new_mode = input("Enter new mode (ai_vs_human, ai_vs_ai, ai_vs_random): ")
+                if new_mode in ['ai_vs_human', 'ai_vs_ai', 'ai_vs_random']:
+                    mode = new_mode
+                else:
+                    print("Invalid mode. Please choose a valid mode.")
+            elif choice == '3':
+                continue_running = False
+            else:
+                print("Invalid choice. Please enter 1, 2, or 3.")
+
+
+
+# Main logic
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Play Chess Game')
+    parser.add_argument('--mode', type=str, default='ai_vs_human',
+                        choices=['ai_vs_human', 'ai_vs_ai', 'ai_vs_random'],
+                        help='Choose game mode: ai_vs_human, ai_vs_ai, or ai_vs_random')
+    parser.add_argument('--continuous', action='store_true',
+                        help='Run continuous games. If not set, run a single game.')
+    parser.add_argument('--verbose', action='store_true',
+                        help='Enable verbose output. If set, print the board after each move.')
+
+    args = parser.parse_args()
+
+    chess_model = ChessAI()
+
+    if args.continuous:
+        run_continuous_games(chess_model, initial_mode=args.mode, verbose=args.verbose)
+    else:
+        play_chess_game(chess_model, mode=args.mode)
